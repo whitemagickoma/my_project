e41e8456682a515ba478eb954f61de92a3c7c9af
Kamron <xajtkulovkoma@gmail.com>
2022-10-10 10:24:56 +0500

    next commit

--- product/views.py
+++ product/views.py
@@ -1,4 +1,15 @@
 from django.shortcuts import render
 
+posts = [
+    {
+        'author': 'Kamron',
+        'name': 'coral-mine',
+        'amount': '5'
+    }
+]
+
 def home(request):
-    return render(request, 'product/index.html')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'product/index.html', context)
